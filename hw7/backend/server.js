98ef267a75c1c28797fd462c53a83dbd99c8148a
stanthemaker <stanwong178@gmail.com>
2021-11-23 10:58:16 +0800

    hw7 in class practice done

---
+++ hw7/backend/server.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv-defaults");
+const User = require("./models/user.js");
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const saveUser = async (id, name) => {
+	const existing = await User.findOne({ name });
+	if (existing) throw new Error(`data ${name} exists!!`);
+	try {
+		const newUser = new User({ id, name });
+		console.log("Created user", newUser);
+		return newUser.save();
+	} catch (e) {
+		throw new Error("User creation error: " + e);
+	}
+};
+
+const deleteDB = async () => {
+	try {
+		await User.deleteMany({});
+		console.log("Database deleted");
+	} catch (e) {
+		throw new Error("Database deletion failed");
+	}
+};
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err));
+db.once("open", async () => {
+	await deleteDB();
+	await saveUser(57, "Ric");
+	await saveUser(108, "Sandy");
+	await saveUser(77, "Peter");
+});
