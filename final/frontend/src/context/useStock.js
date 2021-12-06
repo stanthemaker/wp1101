587be328ddf5f06fd0e64683046fb4f31bbb1791
monica0325 <b09901046@ntu.edu.tw>
2021-12-07 00:58:23 +0800

    modify useStock and index.js

--- final/frontend/src/context/useStock.js
+++ final/frontend/src/context/useStock.js
@@ -1,23 +1,71 @@
 // import React from 'react';
 import { createContext, useContext, useState} from 'react';
+import axios from 'axios';
 
 const StockContext = createContext({
-    signedIn: "",
-    userId: "",
-    favorite: {
-        company: [],
+    signedIn: "", //感覺後端會動到的state才放到這 eg message
+    name:"",
+    password:"",
+    user: {//or userName
+        name: "",
+        company: [{
+            name: "",
+            price: "",
+            graph: "",
+            performance: "",
+        }],
         function: [],
+        passedCompany: [],
     },
 
+    addUser: ()=>{}, //register
+    login: ()=>{},
+    initialize: ()=>{}, //after login
     addFavoriteCompany: ()=> {},
     addFunction: ()=>{},
 })
 
 const StockProvider = (props)=>{
+    const [signedIn, setSignedIn] = useState(false);
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser]= useState({});
+
+    const addUser = async () => { //button
+        const {data: {message}} = await axios.post('/stockcalender/register', {
+            name,
+            password,
+        })
+        console.log(message);
+        if (!message) { 
+            setSignedIn(true);
+            setName("");
+            setPassword("");
+        }
+    }
+    const login = async ()=>{
+        const {data: {message}} = await axios.get('/stockcalender/login', { name, password})
+        console.log(message)
+    }
+    const initialize = ()=>{
+        //useEffect?
+    }
+    const addFavoriteCompany = ()=>{
+
+    }
+    const addFunction = ()=>{
+
+    }
+
     return (
         <StockContext.Provider
           value={{
-
+            user,
+            addUser,
+            login,
+            initialize,
+            addFavoriteCompany,
+            addFunction,
           }}
           {...props}
         />
