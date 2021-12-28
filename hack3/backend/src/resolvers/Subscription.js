f8493c9ad53f12137e69e4125f9054009274d489
stanthemaker <stanwong178@gmail.com>
2021-12-28 09:55:11 +0800

    todo 4

---
+++ hack3/backend/src/resolvers/Subscription.js
@@ -0,0 +1,24 @@
+const Subscription = {
+  /**
+   * Subscribe for task creation
+   */
+  taskCreated: {
+    subscribe: (parent, args, { pubSub }) => {
+      return pubSub.asyncIterator("TASK_CREATED");
+    },
+  },
+  /**
+   * Subscribe for task update
+   */
+  taskUpdated: {
+    subscribe: (parent, args, { pubSub }) => {
+      return pubSub.asyncIterator("TASK_UPDATED");
+    },
+  },
+  /**
+   * Subscribe for task deletion
+   */
+  // TODO 6.2 Add taskDeleted resolver
+};
+
+export default Subscription;
