8e9c22f0db278c14c26ca0619249f40ac6f9e403
stanthemaker <stanwong178@gmail.com>
2022-01-11 14:49:17 +0800

    run model, stockInfo enhanced

--- final/backend/routes/api/utils.js
+++ final/backend/routes/api/utils.js
@@ -22,74 +22,111 @@ exports.marketHeadline = async (req, res) => {
 };
 exports.stockInfo = async (req, res) => {
 	const tags = req.query.tags;
-	let companies = [];
+	let companies = [""];
 	for (let i in tags) {
-		console.log(tags[i]);
 		let options = {
 			method: "GET",
-			url: `https://realstonks.p.rapidapi.com/${tags[i]}`,
-			headers: {
-				"x-rapidapi-host": "realstonks.p.rapidapi.com",
-				"x-rapidapi-key": "767b545ad0mshf942c9a40d8b189p100638jsn95d6fb9a3a1b",
-			},
+			url: ` https://statementdog.com/api/v2/fundamentals/${tags[i]}/2017/2022/cf?qbu=true&qf=analysis`,
 		};
 		await axios
 			.request(options)
 			.then(function (response) {
+				if (response.data.hasOwnProperty("error")) {
+					res.status(404).send({ message: response.data.error.message });
+				}
+				const currentprice =
+					response.data["common"]["StockInfo"]["data"][
+						"latest_closing_price"
+					].slice(11);
+				const lastMonthavgPrice =
+					response.data["monthly"]["Price"]["data"].slice(-1)[0][1];
+
+				const changePercentage =
+					(parseFloat(currentprice) - parseFloat(lastMonthavgPrice)) /
+					parseFloat(lastMonthavgPrice);
+
 				companies.push({
 					tag: tags[i],
-					lastPrice: response.data.price,
-					changePercentage: response.data.change_percentage,
+					lastPrice: currentprice,
+					changePercentage: changePercentage,
 				});
 			})
 			.catch(function (error) {
-				res.status(500).send({ message: "server error" });
 				console.error("error: ", error);
+				res.status(500).send({ message: "error" });
 				return;
 			});
 	}
+	// for (let i in tags) {
+	// 	console.log(tags[i]);
+	// 	let options = {
+	// 		method: "GET",
+	// 		url: `https://realstonks.p.rapidapi.com/${tags[i]}`,
+	// 		headers: {
+	// 			"x-rapidapi-host": "realstonks.p.rapidapi.com",
+	// 			"x-rapidapi-key": "767b545ad0mshf942c9a40d8b189p100638jsn95d6fb9a3a1b",
+	// 		},
+	// 	};
+	// 	await axios
+	// 		.request(options)
+	// 		.then(function (response) {
+	// 			companies.push({
+	// 				tag: tags[i],
+	// 				lastPrice: response.data.price,
+	// 				changePercentage: response.data.change_percentage,
+	// 			});
+	// 		})
+	// 		.catch(function (error) {
+	// 			res.status(500).send({ message: "error" });
+	// 			console.error("error: ", error);
+	// 			return;
+	// 		});
+	// }
 	res.status(200).send({ message: "success", companies: companies });
 };
 exports.runModel = async (req, res) => {
 	//there should be limit on the ineq
-	const tag = "AAPL";
-	let model = "P + R > 2";
-	let PE = 0;
-	let ROET4Q = 0;
-	let currentRatio = 0;
-	let GrossMargin = 0;
+	const tags = req.query.tags;
+	const model = req.query.model;
+	let passedCompany = [];
 
-	await request(
-		`https://statementdog.com/api/v2/fundamentals/${tag}/2017/2022/cf?qbu=true&qf=analysis`,
-		(err, response, body) => {
-			const content = JSON.parse(body);
-			// console.log(content);
-			//features below is last quarter
-			PE = content["common"]["LatestValuation"]["data"]["PE"]; //current price
-			ROET4Q = content["quarterly"]["ROET4Q"]["data"].slice(-1)[0][1] / 100;
-			currentRatio =
-				content["quarterly"]["CurrentRatio"]["data"].slice(-1)[0][1] / 100;
-			GrossMargin = content["quarterly"]["GrossMargin"]["data"].slice(-1)[0][1];
-		}
-	);
-	const index = model.indexOf(">");
-	const LHS = Parser.parse(model.slice(0, index - 1).trim());
-	const RHS = Parser.parse(model.slice(index + 1).trim());
-	const LHSvalue = LHS.evaluate({
-		P: PE,
-		R: ROET4Q,
-		G: GrossMargin,
-		C: currentRatio,
-	});
-	const RHSvalue = RHS.evaluate({
-		P: PE,
-		R: ROET4Q,
-		G: GrossMargin,
-		C: currentRatio,
-	});
-	if (LHSvalue > RHSvalue) {
-		console.log("pass!!");
-	} else {
-		console.log("fail");
+	for (let i = 0; i < tags.length; i++) {
+		const options = {
+			method: "GET",
+			url: `https://statementdog.com/api/v2/fundamentals/${tags[i]}/2017/2022/cf?qbu=true&qf=analysis`,
+		};
+		await axios
+			.request(options)
+			.then(function (response) {
+				const PE = response.data["common"]["LatestValuation"]["data"]["PE"]; //latest PE
+				const ROET4Q =
+					response.data["quarterly"]["ROET4Q"]["data"].slice(-1)[0][1] / 100;
+				const currentRatio =
+					response.data["quarterly"]["CurrentRatio"]["data"].slice(-1)[0][1] /
+					100;
+				const GrossMargin =
+					response.data["quarterly"]["GrossMargin"]["data"].slice(-1)[0][1];
+				const inequation = Parser.parse(model);
+				try {
+					const result = inequation.evaluate({
+						P: PE,
+						R: ROET4Q,
+						G: GrossMargin,
+						C: currentRatio,
+					});
+					if (result === true) {
+						passedCompany.push(tags[i]);
+					}
+				} catch (e) {
+					res.status(500).send({ message: "evaluation error" });
+					console.log(e);
+				}
+			})
+			.catch(function (error) {
+				res.status(500).send({ message: "error" });
+				console.error("error: ", error);
+				return;
+			});
 	}
+	res.status(200).send({ message: "success", passedCompany: passedCompany });
 };
