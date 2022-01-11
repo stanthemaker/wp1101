05ddc29b52dc1cf8b298b6439b17d564ee4b3abe
stanthemaker <stanwong178@gmail.com>
2022-01-11 11:42:26 +0800

    solve frontend warning

--- final/frontend/src/Components/model_component/company_card.js
+++ final/frontend/src/Components/model_component/company_card.js
@@ -1,62 +1,62 @@
-import * as React from 'react'
-import Link from '@mui/material/Link'
-import Typography from '@mui/material/Typography'
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
 
-import Stack from '@mui/material/Stack'
-import BedtimeIcon from '@mui/icons-material/Bedtime'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import { useState } from 'react'
-import Card from '@mui/material/Card'
-import Grow from '@mui/material/Grow'
-import Slide from '@mui/material/Slide'
-import styled from 'styled-components'
+import Stack from "@mui/material/Stack";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import Grow from "@mui/material/Grow";
+import Slide from "@mui/material/Slide";
+import styled from "styled-components";
 
 const Space = styled.section`
 height=0.5px;
-`
+`;
 
 const cards = (
-  <Card sx={10}>
-    <Stack
-      spacing={1.5}
-      justifycontent="center"
-      direction="column"
-      align="center"
-    >
-      <Space />
-      <Stack direction="row" spacing={1}>
-        <BedtimeIcon color="primary" />
-        <Typography color="primary" variant="h7">
-          company name
-        </Typography>
-        <Stack justifycontent="flex-end">
-          <FavoriteBorderIcon color="primary" />
-        </Stack>
-      </Stack>
-      <div>
-        <Link color="primary" href="#"></Link>
-      </div>
-    </Stack>
-  </Card>
-)
+	<Card>
+		<Stack
+			spacing={1.5}
+			justifycontent="center"
+			direction="column"
+			align="center"
+		>
+			<Space />
+			<Stack direction="row" spacing={1}>
+				<BedtimeIcon color="primary" />
+				<Typography color="primary" variant="h7">
+					company name
+				</Typography>
+				<Stack justifycontent="flex-end">
+					<FavoriteBorderIcon color="primary" />
+				</Stack>
+			</Stack>
+			<div>
+				<Link color="primary" href="#"></Link>
+			</div>
+		</Stack>
+	</Card>
+);
 
 export default function Model_Card() {
-  return (
-    <Stack spacing={2}>
-      <Typography component="p" variant="h7" color="primary">
-        company pass the analysis
-      </Typography>
-      <Slide
-        direction="up"
-        in={true}
-        in={true}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(true ? { timeout: 1000 } : {})}
-        justifycontent="center"
-      >
-        {cards}
-      </Slide>
-    </Stack>
-  )
+	return (
+		<Stack spacing={2}>
+			<Typography component="p" variant="h7" color="primary">
+				company pass the analysis
+			</Typography>
+			<Slide
+				direction="up"
+				in={true}
+				in={true}
+				style={{ transformOrigin: "0 0 0" }}
+				{...(true ? { timeout: 1000 } : {})}
+				justifycontent="center"
+			>
+				{cards}
+			</Slide>
+		</Stack>
+	);
 }
--- final/frontend/src/Components/model_component/company_input.js
+++ final/frontend/src/Components/model_component/company_input.js
@@ -1,74 +1,68 @@
-import * as React from 'react'
-import Box from '@mui/material/Box'
-import Input from '@mui/material/Input'
-import Button from '@mui/material/Button'
-import Stack from '@mui/material/Stack'
-import styled from 'styled-components'
-import { useState } from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import styled from "styled-components";
+import { useState } from "react";
 
 const SmallCaption_up = styled.section`
-  font-size: 1em;
-  font-family: 'Times New Roman';
-  color: black;
-  opacity: 0.8;
-`
-let last = 0
+	font-size: 1em;
+	font-family: "Times New Roman";
+	color: black;
+	opacity: 0.8;
+`;
+let last = 0;
 
-const example = '*please seperate each company name with space key'
-let submit = false
+const example = "*please seperate each company name with space key";
+let submit = false;
 export default function Analysis() {
-  const [company, setCompany] = useState([])
+	const [company, setCompany] = useState([]);
 
-  const addCompany = (e) => {
-    if (e.key === 'Enter') {
-      setCompany([...company, { id: last, content: e.target.value }])
-      // e.target.value = ''
-      submit = true
-      e.preventDefault()
-    }
-  }
-  return (
-    <Box component="form" noValidate sx={{ mt: 1 }}>
-      <Stack spacing={2} direction="column">
-        <Stack spacing={3} direction="row">
-          <Input
-            required
-            fullWidth
-            id="function"
-            label="My function"
-            placeholder="enter company abbreviation"
-            autoComplete="function"
-            autoFocus
-            onKeyPress={addCompany}
-          />
-          {/* <Button variant="contained" disableElevation>
+	const addCompany = (e) => {
+		if (e.key === "Enter") {
+			setCompany([...company, { id: last, content: e.target.value }]);
+			// e.target.value = ''
+			submit = true;
+			e.preventDefault();
+		}
+	};
+	return (
+		<Box component="form" noValidate sx={{ mt: 1 }}>
+			<Stack spacing={2} direction="column">
+				<Stack spacing={3} direction="row">
+					<Input
+						required
+						fullWidth
+						id="function"
+						label="My function"
+						placeholder="enter company abbreviation"
+						autoComplete="function"
+						autoFocus
+						onKeyPress={addCompany}
+					/>
+					{/* <Button variant="contained" disableElevation>
             Submit
           </Button> */}
-        </Stack>
-        <SmallCaption_up>company to be analysized :</SmallCaption_up>
-        <Stack spacing={1}>
-          {company.map((c) => (
-            <SmallCaption_up id={c.id}>{c.content}</SmallCaption_up>
-          ))}
-        </Stack>
-        <>
-          {submit ? (
-            <>
-              <SmallCaption_up>company to be analysized :</SmallCaption_up>
-              <Stack spacing={1}>
-                {company.map((c) => (
-                  <SmallCaption_up id={c.id}>{c.content}</SmallCaption_up>
-                ))}
-              </Stack>
-              <Button variant="contained" disableElevation>
-                Start
-              </Button>
-            </>
-          ) : (
-            <></>
-          )}
-        </>
-      </Stack>
-    </Box>
-  )
+				</Stack>
+				<>
+					{submit ? (
+						<>
+							<SmallCaption_up>company to be analysized :</SmallCaption_up>
+							<Stack spacing={1}>
+								{company.map((c) => (
+									<SmallCaption_up id={c.id}>{c.content}</SmallCaption_up>
+								))}
+							</Stack>
+							<Button variant="contained" disableElevation>
+								Start
+							</Button>
+						</>
+					) : (
+						<></>
+					)}
+				</>
+			</Stack>
+		</Box>
+	);
 }
