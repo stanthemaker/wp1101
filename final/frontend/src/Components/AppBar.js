ef8de6e89b79bdf54a34dcfdd5b9497d061ba6f6
stanthemaker <stanwong178@gmail.com>
2022-01-12 11:36:58 +0800

    model page 90% done

--- final/frontend/src/Components/AppBar.js
+++ final/frontend/src/Components/AppBar.js
@@ -1,72 +1,74 @@
-import * as React from 'react';
+import * as React from "react";
 import clsx from "clsx";
-import { useHistory, Link } from 'react-router-dom';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
+import { useHistory, Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
 // import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 // import Menu from '@mui/material/Menu';
 // import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 // import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 // import Tooltip from '@mui/material/Tooltip';
 // import MenuItem from '@mui/material/MenuItem';
 
 // const pages = ['My Favorote', 'Model', 'Home'];
 // const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-import {useStock} from "../context/useStock"
+import { useStock } from "../context/useStock";
 
-const ResponsiveAppBar = ({children}) => {
-  let history = useHistory();
-  const {signedIn} = useStock()
-  // const history = useHistory();
-  // const [anchorElNav, setAnchorElNav] = React.useState(null);
-  // const [anchorElUser, setAnchorElUser] = React.useState(null);
+const ResponsiveAppBar = ({ children }) => {
+	let history = useHistory();
+	const { signedIn } = useStock();
+	// const history = useHistory();
+	// const [anchorElNav, setAnchorElNav] = React.useState(null);
+	// const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  function handleChangePage(page){
-    console.log(`change to ${page}`)
-    history.push(page);
-    // history.pushState(page)
-    
-  }
-  // const handleOpenNavMenu = (event) => {
-  //   setAnchorElNav(event.currentTarget);
-  // };
-  // const handleOpenUserMenu = (event) => {
-  //   setAnchorElUser(event.currentTarget);
-  // };
+	function handleChangePage(page) {
+		console.log(`change to ${page}`);
+		history.push(page);
+		// history.pushState(page)
+	}
+	// const handleOpenNavMenu = (event) => {
+	//   setAnchorElNav(event.currentTarget);
+	// };
+	// const handleOpenUserMenu = (event) => {
+	//   setAnchorElUser(event.currentTarget);
+	// };
 
-  // const handleCloseNavMenu = () => {
-  //   setAnchorElNav(null);
-  // };
+	// const handleCloseNavMenu = () => {
+	//   setAnchorElNav(null);
+	// };
 
-  // const handleCloseUserMenu = () => {
-  //   setAnchorElUser(null);
-  // };
+	// const handleCloseUserMenu = () => {
+	//   setAnchorElUser(null);
+	// };
 
-  return (
-    <div>
-    <AppBar position="static" style={{backgroundColor:"#000000", display: signedIn? "":"none" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'flex', md: 'flex' } }}
-          >
-            <div
-              style={{cursor: "pointer"}}
-              onClick={()=> history.push("/")}
-            >
-            StocKalendar
-            </div>
-          </Typography>
+	return (
+		<div>
+			<AppBar
+				position="static"
+				style={{ backgroundColor: "#000000", display: signedIn ? "" : "none" }}
+			>
+				<Container maxWidth="xl">
+					<Toolbar disableGutters>
+						<Typography
+							variant="h6"
+							noWrap
+							component="div"
+							sx={{ mr: 2, display: { xs: "flex", md: "flex" } }}
+						>
+							<div
+								style={{ cursor: "pointer" }}
+								onClick={() => history.push("/")}
+							>
+								StocKalendar
+							</div>
+						</Typography>
 
-          {/* <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}> */}
-            {/* <IconButton
+						{/* <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}> */}
+						{/* <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
@@ -76,7 +78,7 @@ const ResponsiveAppBar = ({children}) => {
             >
               <MenuIcon />
             </IconButton> */}
-            {/* <Menu
+						{/* <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
@@ -94,17 +96,17 @@ const ResponsiveAppBar = ({children}) => {
                 display: { xs: 'block', md: 'none' },
               }}
             > */}
-              {/* {pages.map((page) => (
+						{/* {pages.map((page) => (
                 <MenuItem key={page} onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))} */}
-              {/* <MenuItem key="myfavorite" onClick={handleCloseNavMenu}>
+						{/* <MenuItem key="myfavorite" onClick={handleCloseNavMenu}>
                   <Typography textAlign="center">MY FAVORITE</Typography>
                 </MenuItem> */}
-            {/* </Menu> */}
-          {/* </Box> */}
-          {/* <Typography
+						{/* </Menu> */}
+						{/* </Box> */}
+						{/* <Typography
             variant="h6"
             noWrap
             component="div"
@@ -112,8 +114,8 @@ const ResponsiveAppBar = ({children}) => {
           >
             LOGO
           </Typography> */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
-            {/* {pages.map((page) => (
+						<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "flex" } }}>
+							{/* {pages.map((page) => (
               <Button
                 key={page}
                 onClick={handleCloseNavMenu}
@@ -122,42 +124,39 @@ const ResponsiveAppBar = ({children}) => {
                 {page}
               </Button>
             ))} */}
-            <Button
-              key="MY FAVORITE"
-              component={Link}
-              to="/myfavorite"
-              onClick={handleChangePage("/myfavorite")}
-              sx={{ my: 2, color: 'white', display: 'flex' }}
-            >
-              MY FAVORITE
-            </Button>
-            <Button
-              key="MODEL"
-              component={Link}
-              to="/model"
-              onClick={handleChangePage("/model")}
-              sx={{ my: 2, color: 'white', display: 'flex' }}
-            >
-              MODEL
-            </Button>
-            <Button
-              key="MAIN"
-              component={Link}
-              to="/"
-              onClick={handleChangePage("/")}
-              sx={{ my: 2, color: 'white', display: 'flex' }}
-            >
-              MAIN
-            </Button>
-            
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-    <div>
-      {children}
-    </div>
-    </div>
-  );
+							<Button
+								key="MY FAVORITE"
+								component={Link}
+								to="/myfavorite"
+								onClick={() => history.push("/myfavorite")}
+								sx={{ my: 2, color: "white", display: "flex" }}
+							>
+								MY FAVORITE
+							</Button>
+							<Button
+								key="MODEL"
+								component={Link}
+								to="/model"
+								onClick={() => history.push("/model")}
+								sx={{ my: 2, color: "white", display: "flex" }}
+							>
+								MODEL
+							</Button>
+							<Button
+								key="MAIN"
+								component={Link}
+								to="/"
+								onClick={() => history.push("/")}
+								sx={{ my: 2, color: "white", display: "flex" }}
+							>
+								MAIN
+							</Button>
+						</Box>
+					</Toolbar>
+				</Container>
+			</AppBar>
+			<div>{children}</div>
+		</div>
+	);
 };
 export default ResponsiveAppBar;
--- final/frontend/src/Components/model_component/company_card.js
+++ final/frontend/src/Components/model_component/company_card.js
@@ -12,62 +12,62 @@ import Card from "@mui/material/Card";
 import Grow from "@mui/material/Grow";
 import Slide from "@mui/material/Slide";
 import styled from "styled-components";
-import { useStock } from "../../context/useStock"
+import { useStock } from "../../context/useStock";
 
 const Space = styled.section`
 height=0.5px;
 `;
 
-const cards = (
-	<Card>
-		<Stack
-			spacing={1.5}
-			justifycontent="center"
-			direction="column"
-			align="center"
-		>
-			<Space />
-			<Stack direction="row" spacing={1}>
-				<BedtimeIcon color="primary" />
-				<Typography color="primary" variant="h7">
-					company name
-				</Typography>
-				<Stack justifycontent="flex-end">
-					<FavoriteBorderIcon color="primary" />
-				</Stack>
-			</Stack>
-			<div>
-				<Link color="primary" href="#"></Link>
-			</div>
-		</Stack>
-	</Card>
-);
-
 export default function Model_Card() {
-	const {passedcompany} = useStock()
-	const [card, setCards] = useState([]) //card.map(()=><Cards>)?
-	useEffect(()=>{
-		if(!passedcompany){
-			console.log("There's no passed company.")
+	const { passedcompany } = useStock();
+	const [cards, setCards] = useState([]); //card.map(()=><Cards>)?
+	useEffect(() => {
+		if (!passedcompany) {
+			console.log("There's no passed company.");
 		} else {
-			setCards(passedcompany)
+			setCards(passedcompany);
 		}
-	},[passedcompany])
+	}, [passedcompany]);
+
 	return (
 		<Stack spacing={2}>
 			<Typography component="p" variant="h7" color="primary">
-				company pass the analysis
+				Passed Companies:
 			</Typography>
-			<Slide
-				direction="up"
-				in={true}
-				in={true}
-				style={{ transformOrigin: "0 0 0" }}
-				{...(true ? { timeout: 1000 } : {})}
-				justifycontent="center"
-			>
-				{cards}
-			</Slide>
+
+			{cards.map((card, index) => (
+				<Slide
+					direction="up"
+					in={true}
+					in={true}
+					style={{ transformOrigin: "0 0 0" }}
+					{...(true ? { timeout: 1000 } : {})}
+					justifycontent="center"
+				>
+					<Card>
+						<Stack
+							spacing={1.5}
+							justifycontent="center"
+							direction="column"
+							align="center"
+						>
+							<Space />
+							<Stack direction="row" spacing={1}>
+								<Space />
+								<Stack justifycontent="flex-end">
+									<FavoriteBorderIcon color="primary" onClick={() => {}} />
+								</Stack>
+								<Typography color="primary" variant="h7" key={index}>
+									{card}
+								</Typography>
+							</Stack>
+							<div>
+								<Link color="primary" href="#"></Link>
+							</div>
+						</Stack>
+					</Card>
+				</Slide>
+			))}
 		</Stack>
 	);
 }
